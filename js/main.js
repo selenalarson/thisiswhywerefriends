972d4109ce6fe7b2eca95a8ccc1fd216df6cf477
Selena Larson <selenalarson@gmail.com>
2015-06-07 14:13:06 -0700

    twitter button

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 	var reasons = [
-		{'text': 'Hello', 'image': 'http://media.giphy.com/media/xQrz8krDeKXIs/giphy.gif'},
+		{'text': 'You also eat brunch at 2 p.m.', 'image': 'http://media.giphy.com/media/xQrz8krDeKXIs/giphy.gif'},
 		{'text':'World', 'image':'http://media.giphy.com/media/xQrz8krDeKXIs/giphy.gif'},
 		{'text':'Because you teach me to code', 'image':'http://media.giphy.com/media/xQrz8krDeKXIs/giphy.gif'},
 		{'text':'We both freak out about dogs', 'image':'http://media.giphy.com/media/xQrz8krDeKXIs/giphy.gif'},
 	];
-	var reason = reasons[Math.floor(Math.random() * reasons.length)];
+	if (window.location.hash){
+		var id = window.location.hash.replace('#','');
+	} else{
+		var id = Math.floor(Math.random() * reasons.length);
+	}
+	var reason = reasons[id];
 	$('#reason').text(reason.text);
 	$('#reason-image').html('<img src="' + reason.image + '">');
+	$('.twitter-share-button').attr('data-text','This is why we\'re friends: ' + reason.text);
+	$('.twitter-share-button').attr('data-url','http://thisiswhywerefriends.com/#' + id);
 });
